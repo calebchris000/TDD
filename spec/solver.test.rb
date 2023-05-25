d49fec06a8bd04726e78542b7d6671e9da084dcc
Caleb Nwaizu <calebchris000@gmail.com>
2023-05-25 18:39:40 +0100

    Create test for Solver

---
+++ spec/solver.test.rb
@@ -0,0 +1,10 @@
+require_relative '../solver'
+require 'RSpec'
+
+RSpec.describe Solver do
+    describe '#Factorial Test' do
+        it 'should return the factorial of a number' do
+            
+        end
+    end
+end
