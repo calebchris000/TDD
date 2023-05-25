50039c440b487a4ed3652a2c5a29748130edff3f
Caleb Nwaizu <calebchris000@gmail.com>
2023-05-25 19:02:31 +0100

    Add reverse string method

--- solver.rb
+++ solver.rb
@@ -1,5 +1,17 @@
 
-
 class Solver
+  def factorial(num)
+    return 1 if [0, 1].include?(num)
 
+    @factor = 1
+    while num > 1
+      @factor *= num
+      num -= 1
+    end
+    @factor
+  end
+
+  def reverse(str)
+    str.reverse
+  end
 end
