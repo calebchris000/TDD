3ada9276f7a8cca875b5eb84da0e511c30d1edcd
Caleb Nwaizu <calebchris000@gmail.com>
2023-05-25 19:02:48 +0100

    Rubocop refactor

--- spec/solver.test.rb
+++ spec/solver.test.rb
@@ -1,27 +1,29 @@
+# frozen_string_literal: true
+
 require_relative '../solver'
 require 'RSpec'
 
 RSpec.describe Solver do
-    describe '#Factorial Test' do
-        it 'should return the factorial of a number' do
-            solver = Solver.new
-            expect(solver.factorial(5)).to eq(120)
-        end
-    end
-    describe '#String Reverse Test' do
-    it 'should reverse a word' do
-        solver = Solver.new
-        word = 'hello'
-        reversed_word = 'olleh'
-        expect(solver.reverse(word)).to eq(reversed_word)
+  describe '#Factorial Test' do
+    it 'should return the factorial of a number' do
+      solver = Solver.new
+      expect(solver.factorial(5)).to eq(120)
     end
   end
-  describe "#Fizzbuzz Test" do
-    it "should correctly execute fizzbuzz" do
-        solver = Solver.new
-        number = 15
-        returned_string = 'fizzbuzz'
-        expect(solver.fizzbuzz(number)).to eq(returned_string)
+  describe '#String Reverse Test' do
+    it 'should reverse a word' do
+      solver = Solver.new
+      word = 'hello'
+      reversed_word = 'olleh'
+      expect(solver.reverse(word)).to eq(reversed_word)
+    end
+  end
+  describe '#Fizzbuzz Test' do
+    it 'should correctly execute fizzbuzz' do
+      solver = Solver.new
+      number = 15
+      returned_string = 'fizzbuzz'
+      expect(solver.fizzbuzz(number)).to eq(returned_string)
     end
   end
 end
