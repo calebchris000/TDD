a3d1e641bd981cbe02d4f92fab9650a987af51db
theophilus doku <theodoku89@gmail.com>
2023-05-25 20:45:24 +0000

    Add fizzbuzz method to solver.rb

--- solver.rb
+++ solver.rb
@@ -1,4 +1,3 @@
-
 class Solver
   def factorial(num)
     return 1 if [0, 1].include?(num)
@@ -13,5 +12,17 @@ class Solver
 
   def reverse(str)
     str.reverse
+  end
+
+  def fizzbuzz(num)
+    if num % 3 == 0 && num % 5 == 0
+      "fizzbuzz"
+    elsif num % 3 == 0
+      "fizz"
+    elsif num % 5 == 0
+      "buzz"
+    else
+      num.to_s
+    end
   end
 end
